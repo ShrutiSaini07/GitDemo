f3f78102b1fb06febaf0d0db37f297856abfe4c5
Shruti Saini <shrutisaini2114@gmail.com>
2024-02-20 01:50:00 +0530

    feat b commit

--- ui/uipage.dart
+++ ui/uipage.dart
@@ -1,1 +1,27 @@
-var ui = "shrutiScreen";
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Button Example'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // Handle button press
+              print('Button pressed');
+            },
+            child: Text('Press me'),
+          ),
+        ),
+      ),
+    );
+  }
+}
